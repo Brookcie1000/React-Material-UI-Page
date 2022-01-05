e62c595ae08ea066d0a6aab3b114ba971177f1f3
James Brooks <jamescbrooks2020@gmail.com>
2022-01-05 14:04:56 +1100

    Created a webpage with a dynamic server.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { green } from '@mui/material/colors'
+
+const theme = createTheme({
+  palette: {
+    primary: green
+  },
+  typography: {
+    fontFamily: 'Raleway',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700
+  }
+})
 
 function App() {
-  return (
+  return (    
+    <ThemeProvider theme={theme}>
     <Router>
       <Switch>
         <Route exact path="/">
@@ -13,7 +29,8 @@ function App() {
           <Create />
         </Route>
       </Switch>
-    </Router>
+    </Router> 
+    </ThemeProvider>   
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import AcUnitRoundedIcon from '@mui/icons-material/AcUnitRounded'
+import { TextField, Radio, RadioGroup, FormControlLabel, FormLabel } from '@mui/material'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Create() {
+  const history = useHistory()
+  const [name, setName] = useState('')
+  const [favFood, setFavFood] = useState('')
+  const [nameError, setNameError] = useState(false)
+  const [foodError, setFoodError] = useState(false)
+  const [category, setCategory] = useState('todos')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (name === '') {
+      setNameError(true)
+
+    } else {
+      setNameError(false)
+    }
+
+    if (favFood === '') {
+      setFoodError(true)
+
+    } else {
+      setFoodError(false)
+    }
+
+    if (name && favFood) {
+      console.log(name, favFood, category)
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({name, favFood, category})
+      })
+      .then(() => history.push('/'))
+    }
+
+  }
+
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+      variant="h6"
+      color="textSecondary"
+      component="h2"
+      gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <TextField
+          sx={
+            {marginTop: 2,
+            marginBottom: 2,
+            display: 'block'}          
+          }
+          label="Name"
+          variant="outlined"
+          color='secondary'
+          fullWidth
+          required
+          onChange={(e) => setName(e.target.value)}
+          error={nameError}
+        />
+
+        <TextField
+          sx={
+            {marginTop: 2,
+            marginBottom: 2,
+            display: 'block'}          
+          }
+          label="Favourite Food"
+          variant="outlined"
+          color='secondary'
+          fullWidth
+          multiline
+          rows={4}
+          required
+          onChange={(e) => setFavFood(e.target.value)}
+          error={foodError}
+        />
+        
+      
+      <FormLabel>Note Category</FormLabel>
+        <RadioGroup value={category} onChange={(e) => setCategory(e.target.value)}>
+          <FormControlLabel value="money" control={<Radio />} label="Money" />
+          <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+          <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+          <FormControlLabel value="work" control={<Radio />} label="Work" />
+      </RadioGroup>
+      
+
+      <Button
+      type="submit"
+      color="primary"
+      variant="contained"
+      endIcon={<AcUnitRoundedIcon />}
+      >
+        Submit
+      </Button>
+
+      </form>
+
+    </Container>
   )
 }
